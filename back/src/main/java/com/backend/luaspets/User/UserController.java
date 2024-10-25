45b0f0919f08eb528bc48eb329ea56328f79efc1
JavierAQR <nonamesteam123@gmail.com>
2024-10-25 14:34:04 -0500

    users

--- back/src/main/java/com/backend/luaspets/User/UserController.java
+++ back/src/main/java/com/backend/luaspets/User/UserController.java
@@ -3,7 +3,6 @@ package com.backend.luaspets.User;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
